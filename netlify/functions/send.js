35783b75f51e9eedba7c1545656dc822d676e23f
AlInsaniya <kaloumarko33@gmail.com>
2026-05-24 20:53:47 +0300

    Update send.js

--- netlify/functions/send.js
+++ netlify/functions/send.js
@@ -1,3 +1,5 @@
+const https = require(‘https’);
+
 exports.handler = async (event) => {
 if (event.httpMethod !== ‘POST’) {
 return { statusCode: 405, body: ‘Method Not Allowed’ };
@@ -7,16 +9,28 @@ const { message } = JSON.parse(event.body);
 const token = process.env.TG_TOKEN;
 const chatId = process.env.TG_CHAT;
 
-const response = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-method: ‘POST’,
-headers: { ‘Content-Type’: ‘application/json’ },
-body: JSON.stringify({ chat_id: chatId, text: message, parse_mode: ‘HTML’ })
+const payload = JSON.stringify({
+chat_id: chatId,
+text: message,
+parse_mode: ‘HTML’
 });
 
-const data = await response.json();
-
-return {
-statusCode: 200,
-body: JSON.stringify(data)
-};
+return new Promise((resolve) => {
+const req = https.request({
+hostname: ‘api.telegram.org’,
+path: `/bot${token}/sendMessage`,
+method: ‘POST’,
+headers: {
+‘Content-Type’: ‘application/json’,
+‘Content-Length’: Buffer.byteLength(payload)
+}
+}, (res) => {
+let data = ‘’;
+res.on(‘data’, chunk => data += chunk);
+res.on(‘end’, () => resolve({ statusCode: 200, body: data }));
+});
+req.on(‘error’, (e) => resolve({ statusCode: 500, body: e.message }));
+req.write(payload);
+req.end();
+});
 };
